112c9fc57610b518103f3b4ae571ba6dd011fa04
Kevin Green <kevin@swedenunlimited.com>
2014-10-31 17:11:47 -0400

    injecting the posts, displaying two random words

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -688,5 +688,13 @@ var wordsData = [{
 
 
 Template.wordsList.helpers({
-  words: wordsData
+  words: function() {
+    // return Words.find({}, {sort: {rand:-1}});
+    var array = Words.find().fetch();
+    var randomIndex = Math.floor( Math.random() * array.length );
+    var element = array[randomIndex];
+    return element;
+    // setInterval(function(){return element;}, 3000);
+    console.log(element);
+  }
 });
