7c057fb9cfc7947bec0dca1b8dbfb1497cd4a203
Kevin Green <kevin@swedenunlimited.com>
2014-10-31 16:03:20 -0400

    creating hellolouis functionality

--- client/templates/posts/post_list.js
+++ client/templates/posts/post_list.js
@@ -1,17 +1,692 @@
-var postsData = [
- {
- title: 'Introducing Telescope',
- url: 'http://sachagreif.com/introducing-telescope/'
- },
- {
- title: 'Meteor',
- url: 'http://meteor.com'
- },
- {
- title: 'The Meteor Book',
- url: 'http://themeteorbook.com'
- }
-];
-Template.postsList.helpers({
- posts: postsData
+var wordsData = [{
+  word: '2g1c'
+}, {
+  word: '2 girls 1 cup'
+}, {
+  word: 'acrotomophilia'
+}, {
+  word: 'anal'
+}, {
+  word: 'anilingus'
+}, {
+  word: 'anus'
+}, {
+  word: 'arsehole'
+}, {
+  word: 'ass'
+}, {
+  word: 'asshole'
+}, {
+  word: 'assmunch'
+}, {
+  word: 'auto erotic'
+}, {
+  word: 'autoerotic'
+}, {
+  word: 'babeland'
+}, {
+  word: 'baby batter'
+}, {
+  word: 'ball gag'
+}, {
+  word: 'ball gravy'
+}, {
+  word: 'ball kicking'
+}, {
+  word: 'ball licking'
+}, {
+  word: 'ball sack'
+}, {
+  word: 'ball sucking'
+}, {
+  word: 'bangbros'
+}, {
+  word: 'bareback'
+}, {
+  word: 'barely legal'
+}, {
+  word: 'barenaked'
+}, {
+  word: 'bastardo'
+}, {
+  word: 'bastinado'
+}, {
+  word: 'bbw'
+}, {
+  word: 'bdsm'
+}, {
+  word: 'beaver cleaver'
+}, {
+  word: 'beaver lips'
+}, {
+  word: 'bestiality'
+}, {
+  word: 'bi curious'
+}, {
+  word: 'big black'
+}, {
+  word: 'big breasts'
+}, {
+  word: 'big knockers'
+}, {
+  word: 'big tits'
+}, {
+  word: 'bimbos'
+}, {
+  word: 'birdlock'
+}, {
+  word: 'bitch'
+}, {
+  word: 'black cock'
+}, {
+  word: 'blonde action'
+}, {
+  word: 'blonde on blonde action'
+}, {
+  word: 'blow j'
+}, {
+  word: 'blow your l'
+}, {
+  word: 'blue waffle'
+}, {
+  word: 'blumpkin'
+}, {
+  word: 'bollocks'
+}, {
+  word: 'bondage'
+}, {
+  word: 'boner'
+}, {
+  word: 'boob'
+}, {
+  word: 'boobs'
+}, {
+  word: 'booty call'
+}, {
+  word: 'brown showers'
+}, {
+  word: 'brunette action'
+}, {
+  word: 'bukkake'
+}, {
+  word: 'bulldyke'
+}, {
+  word: 'bullet vibe'
+}, {
+  word: 'bung hole'
+}, {
+  word: 'bunghole'
+}, {
+  word: 'busty'
+}, {
+  word: 'butt'
+}, {
+  word: 'buttcheeks'
+}, {
+  word: 'butthole'
+}, {
+  word: 'camel toe'
+}, {
+  word: 'camgirl'
+}, {
+  word: 'camslut'
+}, {
+  word: 'camwhore'
+}, {
+  word: 'carpet muncher'
+}, {
+  word: 'carpetmuncher'
+}, {
+  word: 'chocolate rosebuds'
+}, {
+  word: 'circlejerk'
+}, {
+  word: 'cleveland steamer'
+}, {
+  word: 'clit'
+}, {
+  word: 'clitoris'
+}, {
+  word: 'clover clamps'
+}, {
+  word: 'clusterfuck'
+}, {
+  word: 'cock'
+}, {
+  word: 'cocks'
+}, {
+  word: 'coprolagnia'
+}, {
+  word: 'coprophilia'
+}, {
+  word: 'cornhole'
+}, {
+  word: 'cum'
+}, {
+  word: 'cumming'
+}, {
+  word: 'cunnilingus'
+}, {
+  word: 'cunt'
+}, {
+  word: 'darkie'
+}, {
+  word: 'date rape'
+}, {
+  word: 'daterape'
+}, {
+  word: 'deep throat'
+}, {
+  word: 'deepthroat'
+}, {
+  word: 'dick'
+}, {
+  word: 'dildo'
+}, {
+  word: 'dirty pillows'
+}, {
+  word: 'dirty sanchez'
+}, {
+  word: 'doggie style'
+}, {
+  word: 'doggiestyle'
+}, {
+  word: 'doggy style'
+}, {
+  word: 'doggystyle'
+}, {
+  word: 'dog style'
+}, {
+  word: 'dolcett'
+}, {
+  word: 'domination'
+}, {
+  word: 'dominatrix'
+}, {
+  word: 'dommes'
+}, {
+  word: 'donkey punch'
+}, {
+  word: 'double dong'
+}, {
+  word: 'double penetration'
+}, {
+  word: 'dp action'
+}, {
+  word: 'eat my ass'
+}, {
+  word: 'ecchi'
+}, {
+  word: 'ejaculation'
+}, {
+  word: 'erotic'
+}, {
+  word: 'erotism'
+}, {
+  word: 'escort'
+}, {
+  word: 'ethical slut'
+}, {
+  word: 'eunuch'
+}, {
+  word: 'faggot'
+}, {
+  word: 'fecal'
+}, {
+  word: 'felch'
+}, {
+  word: 'fellatio'
+}, {
+  word: 'feltch'
+}, {
+  word: 'female squirting'
+}, {
+  word: 'femdom'
+}, {
+  word: 'figging'
+}, {
+  word: 'fingering'
+}, {
+  word: 'fisting'
+}, {
+  word: 'foot fetish'
+}, {
+  word: 'footjob'
+}, {
+  word: 'frotting'
+}, {
+  word: 'fuck'
+}, {
+  word: 'fuck buttons'
+}, {
+  word: 'fudge packer'
+}, {
+  word: 'fudgepacker'
+}, {
+  word: 'futanari'
+}, {
+  word: 'gang bang'
+}, {
+  word: 'gay sex'
+}, {
+  word: 'genitals'
+}, {
+  word: 'giant cock'
+}, {
+  word: 'girl on'
+}, {
+  word: 'girl on top'
+}, {
+  word: 'girls gone wild'
+}, {
+  word: 'goatcx'
+}, {
+  word: 'goatse'
+}, {
+  word: 'gokkun'
+}, {
+  word: 'golden shower'
+}, {
+  word: 'goodpoop'
+}, {
+  word: 'goo girl'
+}, {
+  word: 'goregasm'
+}, {
+  word: 'grope'
+}, {
+  word: 'group sex'
+}, {
+  word: 'g-spot'
+}, {
+  word: 'guro'
+}, {
+  word: 'hand job'
+}, {
+  word: 'handjob'
+}, {
+  word: 'hard core'
+}, {
+  word: 'hardcore'
+}, {
+  word: 'hentai'
+}, {
+  word: 'homoerotic'
+}, {
+  word: 'honkey'
+}, {
+  word: 'hooker'
+}, {
+  word: 'hot chick'
+}, {
+  word: 'how to kill'
+}, {
+  word: 'how to murder'
+}, {
+  word: 'huge fat'
+}, {
+  word: 'humping'
+}, {
+  word: 'incest'
+}, {
+  word: 'intercourse'
+}, {
+  word: 'jack off'
+}, {
+  word: 'jail bait'
+}, {
+  word: 'jailbait'
+}, {
+  word: 'jerk off'
+}, {
+  word: 'jigaboo'
+}, {
+  word: 'jiggaboo'
+}, {
+  word: 'jiggerboo'
+}, {
+  word: 'jizz'
+}, {
+  word: 'juggs'
+}, {
+  word: 'kike'
+}, {
+  word: 'kinbaku'
+}, {
+  word: 'kinkster'
+}, {
+  word: 'kinky'
+}, {
+  word: 'knobbing'
+}, {
+  word: 'leather restraint'
+}, {
+  word: 'leather straight jacket'
+}, {
+  word: 'lemon party'
+}, {
+  word: 'lolita'
+}, {
+  word: 'lovemaking'
+}, {
+  word: 'make me come'
+}, {
+  word: 'male squirting'
+}, {
+  word: 'masturbate'
+}, {
+  word: 'menage a trois'
+}, {
+  word: 'milf'
+}, {
+  word: 'missionary position'
+}, {
+  word: 'motherfucker'
+}, {
+  word: 'mound of venus'
+}, {
+  word: 'mr hands'
+}, {
+  word: 'muff diver'
+}, {
+  word: 'muffdiving'
+}, {
+  word: 'nambla'
+}, {
+  word: 'nawashi'
+}, {
+  word: 'negro'
+}, {
+  word: 'neonazi'
+}, {
+  word: 'nigga'
+}, {
+  word: 'nigger'
+}, {
+  word: 'nig nog'
+}, {
+  word: 'nimphomania'
+}, {
+  word: 'nipple'
+}, {
+  word: 'nipples'
+}, {
+  word: 'nsfw images'
+}, {
+  word: 'nude'
+}, {
+  word: 'nudity'
+}, {
+  word: 'nympho'
+}, {
+  word: 'nymphomania'
+}, {
+  word: 'octopussy'
+}, {
+  word: 'omorashi'
+}, {
+  word: 'one cup two girls'
+}, {
+  word: 'one guy one jar'
+}, {
+  word: 'orgasm'
+}, {
+  word: 'orgy'
+}, {
+  word: 'paedophile'
+}, {
+  word: 'panties'
+}, {
+  word: 'panty'
+}, {
+  word: 'pedobear'
+}, {
+  word: 'pedophile'
+}, {
+  word: 'pegging'
+}, {
+  word: 'penis'
+}, {
+  word: 'phone sex'
+}, {
+  word: 'piece of shit'
+}, {
+  word: 'pissing'
+}, {
+  word: 'piss pig'
+}, {
+  word: 'pisspig'
+}, {
+  word: 'playboy'
+}, {
+  word: 'pleasure chest'
+}, {
+  word: 'pole smoker'
+}, {
+  word: 'ponyplay'
+}, {
+  word: 'poof'
+}, {
+  word: 'poop chute'
+}, {
+  word: 'poopchute'
+}, {
+  word: 'porn'
+}, {
+  word: 'porno'
+}, {
+  word: 'pornography'
+}, {
+  word: 'prince albert piercing'
+}, {
+  word: 'pthc'
+}, {
+  word: 'pubes'
+}, {
+  word: 'pussy'
+}, {
+  word: 'queaf'
+}, {
+  word: 'raghead'
+}, {
+  word: 'raging boner'
+}, {
+  word: 'rape'
+}, {
+  word: 'raping'
+}, {
+  word: 'rapist'
+}, {
+  word: 'rectum'
+}, {
+  word: 'reverse cowgirl'
+}, {
+  word: 'rimjob'
+}, {
+  word: 'rimming'
+}, {
+  word: 'rosy palm'
+}, {
+  word: 'rosy palm and her 5 sisters'
+}, {
+  word: 'rusty trombone'
+}, {
+  word: 'sadism'
+}, {
+  word: 'scat'
+}, {
+  word: 'schlong'
+}, {
+  word: 'scissoring'
+}, {
+  word: 'semen'
+}, {
+  word: 'sex'
+}, {
+  word: 'sexo'
+}, {
+  word: 'sexy'
+}, {
+  word: 'shaved beaver'
+}, {
+  word: 'shaved pussy'
+}, {
+  word: 'shemale'
+}, {
+  word: 'shibari'
+}, {
+  word: 'shit'
+}, {
+  word: 'shota'
+}, {
+  word: 'shrimping'
+}, {
+  word: 'slanteye'
+}, {
+  word: 'slut'
+}, {
+  word: 's&m'
+}, {
+  word: 'smut'
+}, {
+  word: 'snatch'
+}, {
+  word: 'snowballing'
+}, {
+  word: 'sodomize'
+}, {
+  word: 'sodomy'
+}, {
+  word: 'spic'
+}, {
+  word: 'spooge'
+}, {
+  word: 'spread legs'
+}, {
+  word: 'strap on'
+}, {
+  word: 'strapon'
+}, {
+  word: 'strappado'
+}, {
+  word: 'strip club'
+}, {
+  word: 'style doggy'
+}, {
+  word: 'suck'
+}, {
+  word: 'sucks'
+}, {
+  word: 'suicide girls'
+}, {
+  word: 'sultry women'
+}, {
+  word: 'swastika'
+}, {
+  word: 'swinger'
+}, {
+  word: 'tainted love'
+}, {
+  word: 'taste my'
+}, {
+  word: 'tea bagging'
+}, {
+  word: 'threesome'
+}, {
+  word: 'throating'
+}, {
+  word: 'tied up'
+}, {
+  word: 'tight white'
+}, {
+  word: 'tit'
+}, {
+  word: 'tits'
+}, {
+  word: 'titties'
+}, {
+  word: 'titty'
+}, {
+  word: 'tongue in a'
+}, {
+  word: 'topless'
+}, {
+  word: 'tosser'
+}, {
+  word: 'towelhead'
+}, {
+  word: 'tranny'
+}, {
+  word: 'tribadism'
+}, {
+  word: 'tub girl'
+}, {
+  word: 'tubgirl'
+}, {
+  word: 'tushy'
+}, {
+  word: 'twat'
+}, {
+  word: 'twink'
+}, {
+  word: 'twinkie'
+}, {
+  word: 'two girls one cup'
+}, {
+  word: 'undressing'
+}, {
+  word: 'upskirt'
+}, {
+  word: 'urethra play'
+}, {
+  word: 'urophilia'
+}, {
+  word: 'vagina'
+}, {
+  word: 'venus mound'
+}, {
+  word: 'vibrator'
+}, {
+  word: 'violet blue'
+}, {
+  word: 'violet wand'
+}, {
+  word: 'vorarephilia'
+}, {
+  word: 'voyeur'
+}, {
+  word: 'vulva'
+}, {
+  word: 'wank'
+}, {
+  word: 'wetback'
+}, {
+  word: 'wet dream'
+}, {
+  word: 'white power'
+}, {
+  word: 'women rapping'
+}, {
+  word: 'wrapping men'
+}, {
+  word: 'wrinkled starfish'
+}, {
+  word: 'xx'
+}, {
+  word: 'xxx'
+}, {
+  word: 'yaoi'
+}, {
+  word: 'yellow showers'
+}, {
+  word: 'yiffy'
+}, {
+  word: 'zoophilia'
+}];
+
+
+
+
+Template.wordsList.helpers({
+  words: wordsData
 });
