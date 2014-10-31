18b5d631a10ca5e937f2181bd51ac05dbace177e
Kevin Green <kevin@swedenunlimited.com>
2014-10-31 17:34:05 -0400

    added a button for reloading the wordz

--- client/main.js
+++ client/main.js
@@ -1,1 +1,21 @@
 Meteor.subscribe('words');
+
+Template.body.events({
+  "click button": function() {
+    Meteor.call("words");
+    console.log('click');
+  }
+})
+
+
+Template.wordsList.helpers({
+  words: function() {
+    // return Words.find({}, {sort: {rand:-1}});
+    var array = Words.find().fetch();
+    var randomIndex = Math.floor( Math.random() * array.length );
+    var element = array[randomIndex];
+    return element;
+    // setInterval(function(){return element;}, 3000);
+    console.log(element);
+  }
+});
