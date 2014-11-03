fe439bea29da63de57a5687371d123c979764848
Kevin Green <kevin@swedenunlimited.com>
2014-11-03 12:57:11 -0500

    updated the structure of the app

--- client/main.js
+++ client/main.js
@@ -6,16 +6,3 @@ Template.body.events({
     console.log('click');
   }
 })
-
-
-Template.wordsList.helpers({
-  words: function() {
-    // return Words.find({}, {sort: {rand:-1}});
-    var array = Words.find().fetch();
-    var randomIndex = Math.floor( Math.random() * array.length );
-    var element = array[randomIndex];
-    return element;
-    // setInterval(function(){return element;}, 3000);
-    console.log(element);
-  }
-});
