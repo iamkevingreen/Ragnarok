5a5c784ad69411ad2b62bfd83c8af9151e53c669
Kevin Green <kevin@swedenunlimited.com>
2014-11-05 10:36:09 -0500

    add additional fields to post types

--- client/controllers/posts/post_submit.js
+++ client/controllers/posts/post_submit.js
@@ -3,7 +3,8 @@ Template.postSubmit.events({
     e.preventDefault();
     var post = {
       url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val()
+      title: $(e.target).find('[name=title]').val(),
+      content: $(e.target).find('[name=content]').val()
     };
     post._id = Posts.insert(post);
     Router.go('postPage', post);
