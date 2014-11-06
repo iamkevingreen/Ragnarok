bde745fecdcc61a87f4611c1203f0df5f44ac8b5
Kevin Green <kevin@swedenunlimited.com>
2014-11-06 15:46:58 -0500

    additional routing, additional post crud

--- client/controllers/posts/post_submit.js
+++ client/controllers/posts/post_submit.js
@@ -1,13 +1,26 @@
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
+
     var post = {
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val(),
       content: $(e.target).find('[name=content]').val()
     };
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+
+    Meteor.call('postInsert', post, function(error, result) {
+
+      // abort and display error
+      if (error)
+        return alert(error.reason);
+
+      // please choose a unique page slug to prevent duplication
+      if (result.postExists)
+        alert('This link has already beeen posted');
+
+
+      Router.go('postPage', {_id: result._id});
+    })
   }
 });
 
