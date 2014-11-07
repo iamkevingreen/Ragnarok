26ba687dc6c6260442705c694465f678fb4e7e07
Kevin Green <kevin@swedenunlimited.com>
2014-11-07 12:10:20 -0500

    fudge it custom editor it is

--- client/controllers/posts/post_submit.js
+++ client/controllers/posts/post_submit.js
@@ -28,4 +28,28 @@ Template.postSubmit.rendered = function() {
   $('.datepicker').pickadate({
     format: 'yyyy-mm-dd'
   });
+
+
+  $('#editControls a').click(function(e) {
+    switch($(this).data('role')) {
+      case 'h1':
+      case 'h2':
+      case 'p':
+        document.execCommand('formatBlock', false, $(this).data('role'));
+        break;
+      default:
+        document.execCommand($(this).data('role'), false, null);
+        break;
+      }
+    update_output();
+  })
+
+  $('#editor').bind('blur keyup paste copy cut mouseup', function(e) {
+    update_output();
+  })
+
+  function update_output() {
+    $('#output').val($('#editor').html());
+  }
+
 };
